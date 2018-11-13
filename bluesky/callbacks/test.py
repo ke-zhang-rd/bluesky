d03ad7735de25737f8c246f366a528c51b8985e7
ke-zhang-rd <kz2249@columbia.edu>
2018-11-12 23:12:15 -0500

    use scan instead of count

--- bluesky/callbacks/test.py
+++ bluesky/callbacks/test.py
@@ -1,23 +1,22 @@
 from bluesky import RunEngine
-from bluesky.callbacks.best_effort import BestEffortCallback
-from bluesky.callbacks.mpl_plotting import scatter_factory
-from bluesky.callbacks import Runrouter
+from bluesky.callbacks.callback_factories import grid_factory
+from bluesky.callbacks.callback_factories import scatter_factory
+from bluesky.callbacks.core import RunRouter
 from bluesky.utils import install_kicker
 from databroker import Broker
 from bluesky.utils import ProgressBarManager
-from ophyd.sim import det1, det2  # two simulated detectors
-from bluesky.plans import count
+from ophyd.sim import det4, motor1, motor2
+from bluesky.plans import scan
+from bluesky.plans import grid_scan
 
 RE = RunEngine({})
-bec = BestEffortCallback()
-RE.subscribe(bec)
 install_kicker()
 db = Broker.named('temp')
 RE.subscribe(db.insert)
 RE.waiting_hook = ProgressBarManager()
-dets = [det1, det2]   # a list of any number of detectors
 
 callback_factories = [scatter_factory]
-router = Runrouter(callback_factories)
+router = RunRouter(callback_factories)
 RE.subscribe(router)
-RE(count(dets))
+dets = [det4]   # a list of any number of detectors
+RE(grid_scan(dets, motor1, -1.5, 1.5, 3,motor2, -0.1, 0.1, 5, False))
